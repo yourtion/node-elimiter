9759aa63621d19c73abede4eb06bfeda36ba913e
YourtionGuo <yourtion@gmail.com>
2018-07-27 09:44:53 +0800

    refactor: update get and remove acquire

--- src/index.ts
+++ src/index.ts
@@ -8,9 +8,14 @@ function microtime() {
   return time + diff[0] * 1e6 + Math.round(diff[1] * 1e-3);
 }
 
-export interface IOptions {
+export interface IOpt {
+  id?: string;
   max?: number;
   duration?: number;
+}
+
+export interface IOptions extends IOpt {
+  namespace?: string;
 }
 
 export interface IResult {
@@ -26,59 +31,52 @@ export default class ELimiter {
   private id: string;
   private max: number;
   private duration: number;
-  private key: string;
+  private namespace: string;
 
-  constructor(db: any, id: string, opts: IOptions) {
-    this.id = id;
+  constructor(db: any, opts: IOptions) {
+    this.id = opts.id || "";
     this.db = db;
     assert(this.id, ".id required");
     assert(this.db, ".db required");
     this.max = opts.max || 3600;
     this.duration = opts.duration || 3600000;
-    this.key = "limit:" + this.id;
+    this.namespace = opts.namespace || "limit";
   }
 
   [util.inspect.custom]() {
     return `<Limiter id="${this.id}" duration=${this.duration} max=${this.max} />`;
   }
 
-  get(): Promise<IResult> {
+  get(id?: string): Promise<IResult>;
+  get(opt?: IOpt): Promise<IResult>;
+  get(arg: string | IOpt = {}): Promise<IResult> {
+    const id = (typeof arg === "string" ? arg : arg.id) || this.id;
+    const max = (typeof arg !== "string" && arg.max) || this.max;
+    const duration = (typeof arg !== "string" && arg.duration) || this.duration;
+    assert(id, "id required");
+    assert(max, "max required");
+    assert(duration, "duration required");
+    const key = `${this.namespace}:${id}`;
     const now = microtime();
-    const start = now - this.duration * 1000;
+    const start = now - duration * 1000;
     return this.db
       .multi()
-      .zremrangebyscore(this.key, 0, start)
-      .zcard(this.key)
-      .zadd(this.key, String(now), String(now))
-      .zrange(this.key, 0, 0)
-      .pexpire(this.key, this.duration)
+      .zremrangebyscore(key, 0, start)
+      .zcard(key)
+      .zadd(key, String(now), String(now))
+      .zrange(key, 0, 0)
+      .pexpire(key, duration)
       .exec()
       .then((res: any[]) => {
         const count = parseInt(res[1][1]);
         const oldest = parseInt(res[3][1]);
         return {
           count,
-          remaining: count < this.max ? this.max - count : 0,
-          reset: Math.floor((oldest + this.duration * 1000) / 1000000),
-          resetMs: Math.floor((oldest + this.duration * 1000) / 1000),
-          total: this.max,
+          remaining: count < max ? max - count : 0,
+          reset: Math.floor((oldest + duration * 1000) / 1000000),
+          resetMs: Math.floor((oldest + duration * 1000) / 1000),
+          total: max,
         };
-      });
-  }
-
-  acquire() {
-    const now = microtime();
-    const start = now - this.duration * 1000;
-    return this.db
-      .multi()
-      .zremrangebyscore(this.key, 0, start)
-      .zcard(this.key)
-      .zadd(this.key, String(now), String(now))
-      .pexpire(this.key, this.duration)
-      .exec()
-      .then((res: any[]) => {
-        const count = parseInt(res[1][1]);
-        return this.max - count > 0;
       });
   }
 }
--- src/test.ts
+++ src/test.ts
@@ -24,7 +24,7 @@ describe("Limiter", async function() {
   describe(".total", async function() {
     it("should represent the total limit per reset period", async function() {
       expect.assertions(1);
-      const limit = new Limiter(db, "something", { max: 5 });
+      const limit = new Limiter(db, { id: "something", max: 5 });
       const res = await limit.get();
       expect(res.total).toEqual(5);
     });
@@ -33,7 +33,7 @@ describe("Limiter", async function() {
   describe(".remaining", async function() {
     it("should represent the number of requests remaining in the reset period", async function() {
       expect.assertions(3);
-      const limit = new Limiter(db, "something", { max: 5, duration: 100000 });
+      const limit = new Limiter(db, { id: "something", max: 5, duration: 100000 });
       let res = await limit.get();
       expect(res.remaining).toEqual(5);
 
@@ -48,7 +48,7 @@ describe("Limiter", async function() {
   describe(".reset", async function() {
     it("should represent the next reset time in UTC epoch seconds", async function() {
       expect.assertions(2);
-      const limit = new Limiter(db, "something", { max: 5, duration: 60000 });
+      const limit = new Limiter(db, { id: "something", max: 5, duration: 60000 });
       const res = await limit.get();
       const left = res.reset - Date.now() / 1000;
       expect(left).toBeGreaterThan(0);
@@ -59,7 +59,7 @@ describe("Limiter", async function() {
   describe(".resetMs", async function() {
     it("should represent the next reset time in UTC epoch milliseconds", async function() {
       expect.assertions(3);
-      const limit = new Limiter(db, "something", { max: 5, duration: 60000 });
+      const limit = new Limiter(db, { id: "something", max: 5, duration: 60000 });
       const res = await limit.get();
       const left = res.resetMs - Date.now();
       expect(Number.isInteger(left)).toBe(true);
@@ -71,7 +71,7 @@ describe("Limiter", async function() {
   describe("when the limit is exceeded", async function() {
     it("should retain .remaining at 0", async function() {
       expect.assertions(3);
-      const limit = new Limiter(db, "something", { max: 2 });
+      const limit = new Limiter(db, { id: "something", max: 2 });
       let res = await limit.get();
       expect(res.remaining).toEqual(2);
       res = await limit.get();
@@ -85,7 +85,7 @@ describe("Limiter", async function() {
   describe("when the duration is exceeded", async function() {
     it("should reset", async function() {
       expect.assertions(4);
-      const limit = new Limiter(db, "something", { max: 2, duration: 100 });
+      const limit = new Limiter(db, { id: "something", max: 2, duration: 100 });
 
       let res = await limit.get();
       expect(res.remaining).toEqual(2);
@@ -104,7 +104,7 @@ describe("Limiter", async function() {
   describe("when multiple successive calls are made", async function() {
     it("the next calls should not create again the limiter in Redis", async function() {
       expect.assertions(2);
-      const limit = new Limiter(db, "something", { max: 2, duration: 10000 });
+      const limit = new Limiter(db, { id: "something", max: 2, duration: 10000 });
       const res1q = limit.get();
       const res2q = limit.get();
       const [res1, res2] = await Promise.all([res1q, res2q]);
@@ -114,7 +114,7 @@ describe("Limiter", async function() {
 
     it("updating the count should keep all TTLs in sync", async function() {
       expect.assertions(2);
-      const limit = new Limiter(db, "something", { max: 2, duration: 10000 });
+      const limit = new Limiter(db, { id: "something", max: 2, duration: 10000 });
       limit.get(); // All good here.
       await limit.get();
       const res2 = await (db as any)
@@ -134,7 +134,7 @@ describe("Limiter", async function() {
   describe("when trying to decrease before setting value", async function() {
     it("should create with ttl when trying to decrease", async function() {
       expect.assertions(3);
-      const limit = new Limiter(db, "something", { max: 2, duration: 10000 });
+      const limit = new Limiter(db, { id: "something", max: 2, duration: 10000 });
       await db.set("limit:something:count", 1);
       let res = await limit.get();
       expect(res.remaining).toEqual(2);
@@ -154,7 +154,7 @@ describe("Limiter", async function() {
     for (var i = 0; i < clientsCount; ++i) {
       const db2 = new Redis();
       rediss.push(db2);
-      limits.push(new Limiter(db2, "something", { duration: 10000, max }));
+      limits.push(new Limiter(db2, { id: "something", duration: 10000, max }));
     }
 
     it("should prevent race condition and properly set the expected value", function(done) {
@@ -197,7 +197,7 @@ describe("Limiter", async function() {
   describe("when limiter is called in parallel by multiple clients", async function() {
     let max = 6;
     const db2 = new Redis();
-    const limiter = new Limiter(db2, "asyncsomething", { max, duration: 10000 });
+    const limiter = new Limiter(db2, { id: "asyncsomething", max, duration: 10000 });
 
     it("should set the count properly without race conditions", async function() {
       expect.assertions(max + 1);
@@ -214,29 +214,96 @@ describe("Limiter", async function() {
     });
   });
 
-  describe("test acquire", async function() {
-    it("simple acquire", async function() {
+  describe("when get is called with max option", function() {
+    it("should represent the custom total limit per reset period", async function() {
       expect.assertions(3);
-      const limit = new Limiter(db, "something", { max: 2, duration: 10000 });
-      const [res1, res2, res3] = await Promise.all([limit.acquire(), limit.acquire(), limit.acquire()]);
-      expect(res1).toBe(true);
-      expect(res2).toBe(true);
-      expect(res3).toBeFalsy();
+      const limit = new Limiter(db, { id: "something" });
+
+      let res;
+      res = await limit.get({ max: 10 });
+      expect(res.remaining).toEqual(10);
+      res = await limit.get({ max: 10 });
+      expect(res.remaining).toEqual(9);
+      res = await limit.get({ max: 10 });
+      expect(res.remaining).toEqual(8);
     });
 
-    it("race acquire (new client get counter slower)", async function() {
-      expect.assertions(3);
-      const limit = new Limiter(db, "something", { max: 2, duration: 10000 });
-      const db2 = new Redis();
-      const [res1, res2, res3] = await Promise.all([
-        limit.acquire(),
-        new Limiter(db2, "something", { max: 2, duration: 10000 }).acquire(),
-        limit.acquire(),
-      ]);
-      expect(res1).toBe(true);
-      expect(res2).toBeFalsy();
-      expect(res3).toBe(true);
-      db2.disconnect();
+    it("should take the default limit as fallback", async function() {
+      expect.assertions(4);
+      const limit = new Limiter(db, { id: "something", max: 5 });
+
+      let res = await limit.get({ max: 10 });
+      expect(res.remaining).toEqual(10);
+      res = await limit.get({ max: 10 });
+      expect(res.remaining).toEqual(9);
+      res = await limit.get();
+      expect(res.remaining).toEqual(3);
+      res = await limit.get();
+      expect(res.remaining).toEqual(2);
+    });
+  });
+
+  describe("when get is called with id option", function() {
+    it("should represent the custom total limit per reset period", async function() {
+      expect.assertions(5);
+      const limit = new Limiter(db, { id: "something", max: 5 });
+
+      let res;
+      res = await limit.get("other");
+      expect(res.remaining).toEqual(5);
+      res = await limit.get("other");
+      expect(res.remaining).toEqual(4);
+      res = await limit.get();
+      expect(res.remaining).toEqual(5);
+      res = await limit.get();
+      expect(res.remaining).toEqual(4);
+      res = await limit.get("other");
+      expect(res.remaining).toEqual(3);
+    });
+  });
+
+  describe("when get is called with duration", function() {
+    it("should reset after custom duration", async function() {
+      expect.assertions(2);
+      const limit = new Limiter(db, { id: "something", max: 5 });
+
+      const res = await limit.get({ duration: 10000 });
+      expect(res.remaining).toEqual(5);
+      const left = res.reset - Date.now() / 1000;
+      expect(left).toBeLessThan(10);
+    });
+
+    it("should take the default duration as fallback", async function() {
+      expect.assertions(5);
+      const limit = new Limiter(db, { id: "something", max: 5, duration: 25000 });
+
+      let res = await limit.get({ duration: 10000 });
+      expect(res.remaining).toEqual(5);
+      let left = res.reset - Date.now() / 1000;
+      expect(left).toBeLessThan(10);
+
+      res = await limit.get();
+      expect(res.remaining).toEqual(4);
+      left = res.reset - Date.now() / 1000;
+      expect(left).toBeLessThan(25);
+      expect(left).toBeGreaterThan(10);
+    });
+  });
+
+  describe("when having old data earlier then the duration ", function() {
+    it("the old request data eariler then the duration time should be ignore", async function() {
+      expect.assertions(7);
+      const limit = new Limiter(db, { id: "something", max: 5, duration: 250 });
+      let res = await limit.get();
+      expect(res.remaining).toEqual(5);
+
+      let times = 6;
+      do {
+        res = await limit.get();
+        expect(res.remaining).toBeGreaterThan(0);
+        await sleep(100);
+        times--;
+      } while (times > 0);
     });
   });
 });
